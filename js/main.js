ab242a9ad44047a9e27d40760138d6615b5185b9
Christian Powell <christiant.powell@gmail.com>
2018-04-03 21:14:37 -0500

    Add jQuery fade effect on homepage

--- js/main.js
+++ js/main.js
@@ -10,6 +10,11 @@ $('.img-holder').imageScroll({
     holderMinHeight: 600,
 });
 
+$('h2').mouseover(function () {
+    $(this).fadeOut(100);
+    $(this).fadeIn(600);
+});
+
 var formSubmit = document.getElementById('reservationsform');
 formSubmit.addEventListener('submit', function (event) {
     event.preventDefault();
@@ -17,3 +22,5 @@ formSubmit.addEventListener('submit', function (event) {
     submitElement.innerHTML = '<p>Thanks for your submission. We will respond shortly!</p>';
     document.getElementById('reservationsform').appendChild(submitElement);
 });
+
+
