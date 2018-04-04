680647464bebfc8b24f2724e4ba965bd880cb087
Christian Powell <christiant.powell@gmail.com>
2018-04-03 20:01:19 -0500

    Add JS to show paragraph on submission

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+var submit1 = document.getElementById('submit1');
+submit1.addEventListener('click', function () {
+    var submitElement = document.createElement('article');
+    submitElement.innerHTML = '<p>Thanks for your submission. We will respond shortly!</p>';
+    document.getElementById('reservationsform').appendChild(submitElement);
+});
+
 $(document).ready(function () {
     $('.beefup').beefup();
 });
@@ -9,4 +16,3 @@ $(window).load(function () {
 $('.img-holder').imageScroll({
     holderMinHeight: 600,
 });
-
