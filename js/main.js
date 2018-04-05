a591deeed3165fae5604892a8f3c749843e9ccfd
Christian Powell <christiant.powell@gmail.com>
2018-04-05 10:16:50 -0500

    Fix jQuery code and put scripts at bottom of every page

--- js/main.js
+++ js/main.js
@@ -1,26 +1,31 @@
 $(document).ready(function () {
     $('.beefup').beefup();
-});
 
-$(window).load(function () {
-    $('#mycalendar').monthly();
-});
+    $(window).load(function () {
+        $('#mycalendar').monthly();
+    });
 
-$('.img-holder').imageScroll({
-    holderMinHeight: 600,
-});
+    $('.img-holder').imageScroll({
+        holderMinHeight: 600,
+    });
 
-$('h2').mouseover(function () {
-    $(this).fadeOut(100);
-    $(this).fadeIn(600);
+    $('h2').mouseover(function () {
+        $(this).fadeOut(100);
+        $(this).fadeIn(600);
+    });
 });
 
 
 var formSubmit = document.getElementById('reservationsform');
-formSubmit.addEventListener('submit', function (event) {
-    event.preventDefault();
-    var submitElement = document.createElement('article');
-    submitElement.innerHTML = '<p>Thanks for your submission. We will respond shortly!</p>';
-    document.getElementById('reservationsform').appendChild(submitElement);
-});
 
+if (formSubmit !== null) {
+    formSubmit.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        var submitElement = document.createElement('article');
+
+        submitElement.innerHTML = '<p>Thanks for your submission. We will respond shortly!</p>';
+
+        document.getElementById('reservationsform').appendChild(submitElement);
+    });
+}
