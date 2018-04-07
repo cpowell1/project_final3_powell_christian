680424db79aecc4443a1fb84f9a32335a2ef6025
Christian Powell <christiant.powell@gmail.com>
2018-04-07 12:12:23 -0500

    Add main.js comments

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Beefup is the accordion jQuery Plugin on the service page, #mycalendar is the jQuery calendar plugin on the events page, .img-holder is the jQuery Plugin for parallax scrolling on the homepage, and the targeted h2 fade effect is my custom jQuery code for interactivity for all h2s on the website
 $(document).ready(function () {
     $('.beefup').beefup();
 
@@ -15,7 +16,7 @@ $(document).ready(function () {
     });
 });
 
-
+//This is my custome Javascript code to add a message for when users submit the form
 var formSubmit = document.getElementById('reservationsform');
 
 if (formSubmit !== null) {
